f794cdbba61d1b57e0e24c4783796f4b67730707
sanjayEEGA <sanjay.ega.15@gmail.com>
2022-05-13 11:35:40 -0700

    light

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,15 +1,7 @@
 import { Card} from '@mui/material';
 import React from 'react';
-//import TableElements from '../Elements/TableElements.json'
-// import Checkbox from '@mui/material/Checkbox';
 
-// import CardActions from '@mui/material/CardActions';
-//  import CardContent from '@mui/material/CardContent';
-//import { DataGrid } from '@mui/x-data-grid';
-
-// import Button from '@mui/material/Button';
 import TableElements from '../Elements/TableElements';
-// import SearchIcon from '@mui/icons-material/Search';
 import './Main.css'
 
   
